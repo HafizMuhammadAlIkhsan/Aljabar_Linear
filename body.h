c575eeb2f0d7964ad14f2b0069be152e85b15fcd
Muhamad Haikal Hariyanto <haikaluhuy123@gmail.com>
2024-08-29 11:14:21 +0700

    matrix nol dan matrix perkalian

--- body.h
+++ body.h
@@ -8,6 +8,8 @@ using namespace std;
 
 void createMatrix(int matrix[][100], int baris, int kolom);
 void displayMatrix(int matrix[][100], int baris, int kolom);
+void multipleMatrix(int matrixA[][100], int matrixB[][100], int matrixC[][100], int barisA, int kolomA, int barisB, int kolomB);
+void matriksNol(int matrixNol[][100], int matrix[][100], int baris, int kolom);
 void inversMatriks();
 
 #endif
